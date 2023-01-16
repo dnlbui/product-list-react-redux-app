16e94835d25d00c18e5c138b4d1281fd04865b75
Daniel Bui <dnlbui@proton.me>
2023-01-16 16:41:09 -0600

    took out onchange+select+baseUrl

--- src/features/search_bar/SearchBar.js
+++ src/features/search_bar/SearchBar.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useGetAllProductsQuery } from "../apiSlice";
 
 //function component that renders the searchbar
 const SearchBar = () => {
-  const baseUrl = "localhost:8000/products"
-  const [urlState, setUrl] = useState(`${baseUrl}`)
+  const [queryState, setQuery] = useState('')
   const [categoryState, setCategory ] = useState('');
   const [priceState, setPrice ] = useState('');
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
 
-  let queryUrl = urlState+categoryState+priceState;
+  let queryUrl = queryState+categoryState+priceState;
 
+  const { data } = useGetAllProductsQuery();
+  console.log(data);
   return (
     <div className="container">
       <div className="row">
@@ -21,19 +23,20 @@ const SearchBar = () => {
               placeholder="Search" 
               aria-label="Search input" 
               aria-describedby="button-addon2"
-              onInput={event => setUrl( event.target.value === '' ? baseUrl : baseUrl + "?query=" +event.target.value )}
-              onChange = {dispatch(queryUrl)} />
+              onInput={event => setQuery( event.target.value === '' ? '' : "?query=" + event.target.value )}
+               />
           </div>
         </div>
         <div className="col-lg-3 col-md-4">
           <div className="input-group mb-3">
             <select 
+              defaultValue = ''
               onClick={event => event.target.value === "" ? setCategory("") : setCategory("?price=" + event.target.value)} 
-              onChange = {dispatch(queryUrl)} 
+              
               className="form-select" 
               id="inputGroupSelect01"
             >
-              <option selected value = "">Sort by Category</option>
+              <option value = "">Sort by Category</option>
               <option value="1" >One</option>
               <option value="2" >Two</option>
               <option value="3" >Three</option>
@@ -43,12 +46,13 @@ const SearchBar = () => {
         <div className="col-lg-3 col-md-4">
           <div className="input-group mb-3">
               <select 
+                defaultValue = ''
                 onClick={event => event.target.value === "" ? setPrice("") : setPrice("?price=" + event.target.value)} 
-                onChange = {dispatch(queryUrl)}
+                
                 className="form-select" 
                 id="inputGroupSelect02"
               >
-                <option selected value = "">Sort by Price</option>
+                <option value = "">Sort by Price</option>
                 <option value="lowest" >lowest</option>
                 <option value="highest" >highest</option>
               </select>
