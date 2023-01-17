3b43a92f058d430a286225a2bba8d5fb9283c844
Daniel Bui <dnlbui@proton.me>
2023-01-17 15:51:56 -0600

    added a reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {productsApi} from '../features/apiSlice';
 import {setupListeners} from '@reduxjs/toolkit/query';
+import searchBarReducer from '../features/search_bar/searchBarSlice'
 
 export const store = configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
+    searchBar: searchBarReducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
