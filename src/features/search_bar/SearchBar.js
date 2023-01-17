6648d69ccf693762a5c58c8cabd372ec72e4e15f
Daniel Bui <dnlbui@proton.me>
2023-01-17 13:45:03 -0600

    searchbar now works as expected for local query

--- src/features/search_bar/SearchBar.js
+++ src/features/search_bar/SearchBar.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useGetAllProductsQuery } from "../apiSlice";
+import { useGetProductsQuery } from "../apiSlice";
 
 //function component that renders the searchbar
 const SearchBar = () => {
-  const [queryState, setQuery] = useState('')
+  //local states for search bar
+  const [inputState, setInput] = useState('')
   const [categoryState, setCategory ] = useState('');
   const [priceState, setPrice ] = useState('');
-  //const dispatch = useDispatch();
-
-  let queryUrl = queryState+categoryState+priceState;
-
-  const { data } = useGetAllProductsQuery();
+  //set up redex state
+  const dispatch = useDispatch();
+  //trying out api query
+  const queryValues = inputState+categoryState+priceState;
+  const { data } = useGetProductsQuery(queryValues);
   console.log(data);
+
   return (
     <div className="container">
       <div className="row">
@@ -23,38 +25,42 @@ const SearchBar = () => {
               placeholder="Search" 
               aria-label="Search input" 
               aria-describedby="button-addon2"
-              onInput={event => setQuery( event.target.value === '' ? '' : "?query=" + event.target.value )}
+              onInput={event => setInput( event.target.value === '' ? '' : "&query=" + event.target.value )}
+              
                />
           </div>
         </div>
         <div className="col-lg-3 col-md-4">
           <div className="input-group mb-3">
             <select 
-              defaultValue = ''
-              onClick={event => event.target.value === "" ? setCategory("") : setCategory("?price=" + event.target.value)} 
-              
               className="form-select" 
               id="inputGroupSelect01"
+              defaultValue = ''
+              onChange={event => event.target.value === "" ? setCategory("") : setCategory("&category=" + event.target.value)}
             >
               <option value = "">Sort by Category</option>
-              <option value="1" >One</option>
-              <option value="2" >Two</option>
-              <option value="3" >Three</option>
+              <option value="Music">Music</option>
+              <option value="Shoes" >Shoes</option>
+              <option value="Health" >Health</option>
+              <option value="Grocery" >Grocery</option>
+              <option value="Outdoors" >Outdoors</option>
+              <option value="Jewelery" >Jewelery</option>
+              <option value="Home" >Home</option>
+              <option value="Games" >Games</option>
             </select>
           </div>
         </div>
         <div className="col-lg-3 col-md-4">
           <div className="input-group mb-3">
               <select 
-                defaultValue = ''
-                onClick={event => event.target.value === "" ? setPrice("") : setPrice("?price=" + event.target.value)} 
-                
                 className="form-select" 
                 id="inputGroupSelect02"
+                defaultValue = ''
+                onChange={event => event.target.value === "" ? setPrice("") : setPrice("&price=" + event.target.value)} 
               >
                 <option value = "">Sort by Price</option>
-                <option value="lowest" >lowest</option>
-                <option value="highest" >highest</option>
+                <option value="Lowest" >lowest</option>
+                <option value="Highest" >highest</option>
               </select>
             </div>
         </div>
